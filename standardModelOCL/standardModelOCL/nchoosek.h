380ca9591b9b7bd9a83ffeca1a8d3f7734beb0f4
SND\bjurkovski_cp <SND\bjurkovski_cp@a83551a4-30f6-4d81-a974-c6ced450ddbf>
2010-12-05 17:05:48 +0000

    git-svn-id: https://opencl.svn.codeplex.com/svn@59281 a83551a4-30f6-4d81-a974-c6ced450ddbf

--- standardModelOCL/standardModelOCL/nchoosek.h
+++
@@ -1,8 +0,0 @@
-#ifndef OCL_ST_MODEL_NCHOOSEK_H_
-#define OCL_ST_MODEL_NCHOOSEK_H_
-
-int** nchoosek(const int n, const int k, int& nck);
-int** nchoosekMatrix(const int n, const int m, int& lines);
-int* nchoosekVector(const int n, const int m, int& lines);
-
-#endif
